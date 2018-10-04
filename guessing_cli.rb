8d2835382b0edd06f83c368d0dcf8b5eb9f04c61
Min Hee  Park <minheekangg@gmail.com>
2018-10-04 02:34:39 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,7 +7,8 @@ def run_guessing_game
     puts "Goodbye!"
   end
 
-  if user_response.to_i == (1..6)rand
+  if user_response.to_i == rand(1..6)
     puts "You guessed the correct number!"
   end
+
 end
